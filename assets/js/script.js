ef70dddbc879a26fdf8ed81227e269be90106913
seba <sebadelsor@duocuc.cl>
2024-04-29 09:50:44 -0400

    primer avance

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const obtenerDigimonPromesas =() =>{
+    return new Promise(function(resolve,reject){
+        fetch('https://digimon-api.vercel.app/api/digimon')
+            .then((response) =>{
+                if(!response.ok){
+                    throw new Error("Error");
+                }
+                return response.json();
+            })
+            .then((data) =>{
+                resolve(data);
+            })
+            .catch((Error)=>{
+                reject(error)
+            })
+
+    });
+
+
+}
+
+
+   
+
+
+        obtenerDigimonPromesas()
+        .then((digimones)=>{
+            digimones.map((digimon)=> {
+                console.log(digimon);
+            })
+        }).catch((error)=>{
+            console.log(`El error es: ${error}`);
+        })
